2975b5ff61cb050fa5ea40c26459c0910fa9d91a
TheBotlyNoob <thebotlynoob@gmail.com>
2021-11-05 22:46:50 -0400

    why import????

--- main.ts
+++ main.ts
@@ -4,12 +4,11 @@ import onReady from '@/events/ready.js';
 import { REST } from '@discordjs/rest';
 import { Client, _command, Logger } from '@/types.js';
 import { Routes } from 'discord-api-types/v9';
-import { resolve } from 'node:path';
+import { resolve, dirname } from 'node:path';
 import { spawn as spawnSync } from 'child_process';
 import chalk from 'chalk';
 import fs from 'fs-extra';
-import { fileURLToPath, pathToFileURL } from 'url';
-import { dirname } from 'path';
+import { fileURLToPath, pathToFileURL } from 'node:url';
 
 export const dist = dirname(fileURLToPath(import.meta.url));
 export const root = resolve(dist, '..');
@@ -17,7 +16,9 @@ export const root = resolve(dist, '..');
 process.chdir(dist);
 
 export async function require(path: string): Promise<any> {
-  console.log(await import(String(pathToFileURL(path))));
+  let imported = await import(String(pathToFileURL(path)));
+
+  return imported?.default ?? imported;
 }
 
 const { token, youtubeApiKey } = JSON.parse(
@@ -49,7 +50,7 @@ if (process.env.RESTARTED === 'yes')
 await Promise.all(
   (
     await glob('handlers/**/*.js')
-  ).map(async (handler: string) => await require(`${dist}/${handler}`))
+  ).map(async (handler: string) => (await require(resolve(dist, handler)))())
 );
 
 client.once('ready', async () => await onReady(client));

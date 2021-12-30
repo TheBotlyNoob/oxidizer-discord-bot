fea08d6ec91eefcda22eb96bf42c136b09350183
Jay Jackson <jj@jj.is-a.dev>
2021-12-30 04:49:11 +0000

    t

---
+++ src/logging.rs
@@ -0,0 +1,35 @@
+use std::{
+  env,
+  error::Error,
+  fs,
+  time::{SystemTime, UNIX_EPOCH},
+};
+
+pub(crate) fn init() -> Result<(), Box<dyn Error + Send + Sync>> {
+  let dir =
+    env::var("CARGO_MANIFEST_DIR").unwrap_or(env::current_dir()?.to_string_lossy().to_string());
+
+  fs::create_dir_all(format!("{}/logs", dir,))?;
+
+  simplelog::CombinedLogger::init(vec![
+    simplelog::TermLogger::new(
+      simplelog::LevelFilter::Debug,
+      simplelog::Config::default(),
+      simplelog::TerminalMode::Mixed,
+      simplelog::ColorChoice::Auto,
+    ),
+    simplelog::WriteLogger::new(
+      simplelog::LevelFilter::Debug,
+      simplelog::Config::default(),
+      fs::File::create(format!(
+        "{}/logs/{}.log",
+        dir,
+        SystemTime::now().duration_since(UNIX_EPOCH)?.as_millis()
+      ))
+      .unwrap(),
+    ),
+  ])
+  .expect("Failed to start simplelog");
+
+  Ok(())
+}
--- src/svg.rs
+++ src/svg.rs
@@ -2,27 +2,29 @@ use resvg::render;
 use tiny_skia::Pixmap;
 use usvg::{FitTo, Options, Tree};
 
+pub(crate) const DEFAULT_FONT_SIZE: u8 = 8;
+
 #[derive(Clone, Debug)]
 pub(crate) struct SVG {
   pub(crate) font_size: u8,
   pub(crate) width: u16,
   pub(crate) chars: u8,
-  svg: String,
+  pub(crate) data: String,
 }
 
 impl SVG {
   pub(crate) fn builder(font_size: Option<u8>) -> Self {
     Self {
-      font_size: font_size.unwrap_or(16),
+      font_size: font_size.unwrap_or(DEFAULT_FONT_SIZE),
       width: 0,
       chars: 0,
-      svg: String::new(),
+      data: String::new(),
     }
   }
 
   pub(crate) fn add_emoji<T: AsRef<str>>(mut self, href: T) -> Self {
-    self.svg.push_str(&format!(
-      "  <image class=\"emoji in-svg\" x=\"{}\" y=\"10\" height=\"32\" width=\"32\" href=\"{}\" />\n",
+    self.data.push_str(&format!(
+      "<image class=\"emoji in-svg\" x=\"{}\" y=\"10\" height=\"32\" width=\"32\" href=\"{}\" />",
       self.width,
       href.as_ref()
     ));
@@ -33,46 +35,43 @@ impl SVG {
   }
 
   pub(crate) fn add_text<T: AsRef<str>>(mut self, text: T) -> Self {
-    self.svg.push_str(&format!(
-      "  <text class=\"text in-svg\" x=\"{}\" y=\"31\">{}</text>\n",
+    self.data.push_str(&format!(
+      "<text class=\"text in-svg\" x=\"{}\" y=\"8\">{}</text>",
       self.width,
       text.as_ref()
     ));
 
-    self.width += (text.as_ref().len() as u8 * self.font_size) as u16;
+    self.width += (text.as_ref().len() as u8 * (self.font_size / 2)) as u16;
 
     self
   }
 
   pub(crate) fn add_styles<T: AsRef<str>>(mut self, styles: T) -> Self {
-    self.svg = format!(
-      "
-<style>
-  {}
-</style>
-
-{}
-    ",
-      styles.as_ref(),
-      self.svg
-    );
+    self.data = format!("<style>{}</style> {}", styles.as_ref(), self.data);
 
     self
   }
 
-  pub(crate) fn build(self) -> (String, Pixmap) {
+  pub(crate) fn build(self) -> Built {
     let svg = format!(
       "
-<svg viewBox=\"0 0 {} 50\" xmlns=\"http://www.w3.org/2000/svg\">
+<svg viewBox=\"0 0 {} 20\" xmlns=\"http://www.w3.org/2000/svg\">
+  <style>
+    .in-svg {{
+      font-family: \"Trebuchet MS\" !important;
+      font-size: {}px !important;
+    }}
+  </style>
+  
 {}
 </svg>
 ",
-      self.width, self.svg
+      100 + self.width,
+      self.font_size,
+      self.data
     );
 
-    let mut original_pixmap = Pixmap::new(self.width as u32, 50).unwrap();
-
-    let mut_pixmap = original_pixmap.as_mut();
+    let mut pixmap = Pixmap::new(self.width as u32, 50).unwrap();
 
     render(
       &Tree::from_str(
@@ -86,9 +85,14 @@ impl SVG {
       )
       .expect("Failed to create a tree"),
       FitTo::Original,
-      mut_pixmap,
+      pixmap.as_mut(),
     );
 
-    (svg, mut_pixmap.to_owned())
+    Built { svg, pixmap }
   }
+}
+
+pub(crate) struct Built {
+  pub(crate) svg: String,
+  pub(crate) pixmap: Pixmap,
 }
